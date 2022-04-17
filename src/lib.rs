2f38fc5e07797ba47e8f9f90cc5fe38f063f576d
Peter Ye <yaxollum@gmail.com>
2022-04-17 11:56:05 -0400

    done

--- src/lib.rs
+++ src/lib.rs
@@ -24,11 +24,12 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
                 quantity: "1".to_owned(),
             },
             planned_work_periods: VecDeque::new(),
+            current_date_bedtime: CurrentDateBedtime::default(),
             default_work_sleep_goals: WorkSleepGoals {
                 work_sleep_balance: 70,
                 target_work_count: 6,
                 target_bedtime: Bedtime {
-                    time: NaiveTime::from_hms(11, 0, 0),
+                    time: NaiveTime::from_hms(23, 0, 0),
                     next_day: false,
                 },
                 bedtime_pts_halflife: 30,
@@ -53,6 +54,7 @@ struct Model {
 struct Data {
     current_date: NaiveDate,
     new_task: NewTask,
+    current_date_bedtime: CurrentDateBedtime,
     planned_work_periods: VecDeque<Period>,
     default_work_sleep_goals: WorkSleepGoals,
     work_sleep_data: WorkSleepData,
@@ -65,6 +67,12 @@ struct Refs {}
 struct NewTask {
     name: String,
     quantity: String,
+}
+
+#[derive(Default, Debug)]
+struct CurrentDateBedtime {
+    time: String,
+    is_next_day: bool,
 }
 
 struct Period {
@@ -117,6 +125,13 @@ impl WorkSleepData {
             *current_date - Duration::days(3)
         };
     }
+    fn set_to_default_goals(
+        &mut self,
+        current_date: &NaiveDate,
+        work_sleep_goals: &WorkSleepGoals,
+    ) {
+        self.get_mut_or_create(current_date, work_sleep_goals).goals = work_sleep_goals.clone();
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -166,6 +181,15 @@ impl WorkSleep {
         };
         (work_score + sleep_score).round() as i64
     }
+
+    fn show_score_calc(&self)->String{
+       if let Some(actual_bedtime) = &self.actual_bedtime{
+        format!("<p>Score = Work points * Work periods completed / Target work periods + Sleep points * (1/2)^(|Actual bedtime - Target bedtime|/Bedtime points half-life)</p><p>= {}*{}/{}+{}*(1/2)^({}/{})</p><p>= {}</p>",self.goals.work_sleep_balance,
+        self.actual_work_count,self.goals.target_work_count,
+        
+       100- self.goals.work_sleep_balance,actual_bedtime.abs_diff(&self.goals.target_bedtime),self.goals.bedtime_pts_halflife,self.calc_score()
+    )}else{"(no bedtime data)".to_owned()}
+    }
 }
 
 // ------ ------
@@ -184,8 +208,16 @@ enum Msg {
     FinishedTopTask,
     NewTaskNameChanged(String),
     NewTaskQuantityChanged(String),
+    CurrentDateBedtimeChanged(String),
+    CurrentDateBedtimeNextDayChanged,
+    UpdateBedtime,
     ViewNextWeek,
     ViewPreviousWeek,
+    SetWorkSleepBalance(String),
+    SetTargetWorkCount(String),
+    TargetBedtimeChanged(String),
+    TargetBedtimeNextDayChanged,
+    SetBedtimePointsHalflife(String),
 }
 
 // `update` describes how to handle each `Msg`.
@@ -241,7 +273,6 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
                     &model.data.default_work_sleep_goals,
                 )
                 .actual_work_count += 1;
-            log!(model.data.work_sleep_data);
         }
         Msg::NewTaskNameChanged(s) => {
             model.data.new_task.name = s;
@@ -249,11 +280,77 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
         Msg::NewTaskQuantityChanged(s) => {
             model.data.new_task.quantity = s;
         }
+        Msg::CurrentDateBedtimeChanged(s) => {
+            model.data.current_date_bedtime.time = s;
+        }
+        Msg::CurrentDateBedtimeNextDayChanged => {
+            model.data.current_date_bedtime.is_next_day ^= true
+        }
+        Msg::UpdateBedtime => {
+            if let Ok(time) =
+                NaiveTime::parse_from_str(&model.data.current_date_bedtime.time, "%H:%M")
+            {
+                model
+                    .data
+                    .work_sleep_data
+                    .get_mut_or_create(
+                        &model.data.current_date,
+                        &model.data.default_work_sleep_goals,
+                    )
+                    .actual_bedtime = Some(Bedtime {
+                    time,
+                    next_day: model.data.current_date_bedtime.is_next_day,
+                });
+            }
+        }
         Msg::ViewNextWeek => {
             model.data.work_sleep_data.week_start += Duration::weeks(1);
         }
         Msg::ViewPreviousWeek => {
             model.data.work_sleep_data.week_start -= Duration::weeks(1);
+        }
+        Msg::SetWorkSleepBalance(s) => {
+            model.data.default_work_sleep_goals.work_sleep_balance =
+                s.parse().expect("set work sleep balance");
+
+            model.data.work_sleep_data.set_to_default_goals(
+                &model.data.current_date,
+                &model.data.default_work_sleep_goals,
+            );
+        }
+        Msg::SetTargetWorkCount(s) => {
+            model.data.default_work_sleep_goals.target_work_count =
+                s.parse().expect("set target work count");
+
+            model.data.work_sleep_data.set_to_default_goals(
+                &model.data.current_date,
+                &model.data.default_work_sleep_goals,
+            );
+        }
+        Msg::TargetBedtimeChanged(s) => {
+            if let Ok(time) = NaiveTime::parse_from_str(&s, "%H:%M") {
+                model.data.default_work_sleep_goals.target_bedtime.time = time;
+                model.data.work_sleep_data.set_to_default_goals(
+                    &model.data.current_date,
+                    &model.data.default_work_sleep_goals,
+                );
+            }
+        }
+        Msg::TargetBedtimeNextDayChanged => {
+            model.data.default_work_sleep_goals.target_bedtime.next_day ^= true;
+            model.data.work_sleep_data.set_to_default_goals(
+                &model.data.current_date,
+                &model.data.default_work_sleep_goals,
+            );
+        }
+        Msg::SetBedtimePointsHalflife(s) => {
+            model.data.default_work_sleep_goals.bedtime_pts_halflife =
+                s.parse().expect("set bedtime halflife");
+
+            model.data.work_sleep_data.set_to_default_goals(
+                &model.data.current_date,
+                &model.data.default_work_sleep_goals,
+            );
         }
     }
 }
@@ -284,13 +381,43 @@ fn view_work_sleep_data(model: &Model) -> Node<Msg> {
             ))]]
     ]
 }
+
 fn view_current_date(model: &Model) -> Node<Msg> {
+    div![
+        view_current_date_reality(model),
+        view_current_date_goals(model),
+    ]
+}
+fn view_current_date_reality(model: &Model) -> Node<Msg> {
+    div![
+        h2!["Today"],
+        view_current_date_planning(model),
+        br![],
+        view_current_date_bedtime(model),
+        br![],
+        view_current_date_score_calculation(model),
+    ]
+}
+
+fn view_current_date_score_calculation(model: &Model) -> Node<Msg> {
+    let  calc=if let Some(ws)=model
+    .data
+    .work_sleep_data.data.get(&model.data.current_date){ws
+        .show_score_calc()}else{"(no data)".to_owned()};
+    div![
+        
+        h3!["Score Calculation"],
+        raw![&calc]
+    ]
+}
+
+fn view_current_date_planning(model: &Model) -> Node<Msg> {
     div![
         ul![
             if let Some(wp) = model.data.planned_work_periods.front() {
-                Some(view_first_work_period(&wp.name, wp.id))
+                (view_first_work_period(&wp.name, wp.id))
             } else {
-                None
+                li!["(task list is empty)"]
             },
             model
                 .data
@@ -310,14 +437,88 @@ fn view_current_date(model: &Model) -> Node<Msg> {
                 Msg::NewTaskQuantityChanged(quantity)
             })
         ],
-        input![attrs![
-            At::Type => "range",
-            At::Min => "100",
-            At::Max => "800",
-            At::Step => "50",
-            At::Value => "400",
-        ]],
         button!["Add new task", ev(Ev::Click, |_| Msg::AddNewTask)]
+    ]
+}
+
+fn view_current_date_bedtime(model: &Model) -> Node<Msg> {
+    div![
+        label!["Actual bedtime: "],
+        input![
+            attrs! {At::Type=>"time"},
+            input_ev(Ev::Input, Msg::CurrentDateBedtimeChanged)
+        ],
+        label!["Tomorrow?"],
+        input![
+            attrs! {At::Type=>"checkbox"},
+            input_ev(Ev::Change, |_| Msg::CurrentDateBedtimeNextDayChanged)
+        ],
+        button!["Update", input_ev(Ev::Click, |_| Msg::UpdateBedtime)]
+    ]
+}
+
+fn view_current_date_goals(model: &Model) -> Node<Msg> {
+    div![
+        h2!["Goals"],
+        label![format!(
+            "Work/sleep balance: {} points for work, {} points for sleep",
+            model.data.default_work_sleep_goals.work_sleep_balance,
+            100 - model.data.default_work_sleep_goals.work_sleep_balance
+        )],
+        br![],
+        input![
+            attrs![
+                At::Type => "range",
+                At::Min => "0",
+                At::Max => "100",
+                At::Step => "5",
+                At::Value =>  model.data.default_work_sleep_goals.work_sleep_balance,
+            ],
+            input_ev(Ev::Input, Msg::SetWorkSleepBalance)
+        ],
+        br![],
+        label![format!(
+            "Target work periods: {}",
+            model.data.default_work_sleep_goals.target_work_count
+        )],
+        br![],
+        input![
+            attrs![
+                At::Type => "range",
+                At::Min => "0",
+                At::Max => "20",
+                At::Step => "1",
+                At::Value =>  model.data.default_work_sleep_goals.target_work_count,
+            ],
+            input_ev(Ev::Input, Msg::SetTargetWorkCount)
+        ],
+        br![],
+        label!["Target bedtime: "],
+        input![
+            attrs! {At::Type=>"time",At::Value=>model.data.default_work_sleep_goals.target_bedtime.time.format("%H:%M")},
+            input_ev(Ev::Input, Msg::TargetBedtimeChanged)
+        ],
+        label!["Tomorrow?"],
+        input![
+            attrs! {At::Type=>"checkbox"},
+            input_ev(Ev::Change, |_| Msg::TargetBedtimeNextDayChanged)
+        ],
+        br![],
+        label![format!(
+            "Bedtime points half-life: {} minutes",
+            model.data.default_work_sleep_goals.bedtime_pts_halflife
+        )],
+        br![],
+        input![
+            attrs![
+                At::Type => "range",
+                At::Min => "0",
+                At::Max => "60",
+                At::Step => "1",
+                At::Value =>  model.data.default_work_sleep_goals.bedtime_pts_halflife,
+            ],
+            input_ev(Ev::Input, Msg::SetBedtimePointsHalflife)
+        ],
     ]
 }
 
@@ -327,7 +528,7 @@ fn view_work_sleep_data_one_day(
     is_current_date: bool,
 ) -> Node<Msg> {
     td![
-        IF!(is_current_date=>vec![span!["CURRENT DATE"],br![]]),
+        IF!(is_current_date=>vec![span!["CURRENT DAY"],br![]]),
         button![
             date.to_string(),
             ev(Ev::Click, move |_| Msg::SetCurrentDate(date)),
@@ -338,7 +539,15 @@ fn view_work_sleep_data_one_day(
                 span![format!("Work Completed: {}", ws.actual_work_count)],
                 br![],
                 span![if let Some(actual_bedtime) = &ws.actual_bedtime {
-                    format!("Bedtime: {}", actual_bedtime.time.to_string())
+                    format!(
+                        "Bedtime: {}{}",
+                        actual_bedtime.time.format("%I:%M %p"),
+                        if actual_bedtime.next_day {
+                            " (next day)"
+                        } else {
+                            ""
+                        }
+                    )
                 } else {
                     "No bedtime data".to_owned()
                 }],
